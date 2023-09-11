b9e7647d82fa65d1c434670260801a5578a4e951
Ram Goel <rgoel766@gmail.com>
2023-09-11 22:21:07 +0530

    fixed ts error

--- client/app/lobby/lobby.actions.ts
+++ client/app/lobby/lobby.actions.ts
@@ -1,10 +1,16 @@
 import API from "@/services/api"
 import { lobbyDetailsProps } from "./page"
 import { setInviteLink } from "@/redux/slices/authSlice"
+interface apiResponse {
+    data: dataProps
+}
 
+interface dataProps {
+    inviteLink: string
+}
 export const joinRoom = (data: lobbyDetailsProps, callback: Function) => {
     return (dispatch: Function) => {
-        API.post('/login', data).then((res: object) => {
+        API.post('/login', data).then((res: apiResponse) => {
             dispatch(setInviteLink(res?.data?.inviteLink))
             if (callback) {
                 callback()
