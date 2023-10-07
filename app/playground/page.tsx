1e3bd9be1aa46b02d7d364bfe62121c14ecd3517
Ram Goel <rgoel766@gmail.com>
2023-10-07 20:03:07 +0530

    fizx

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -5,12 +5,6 @@ import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
 
 const Playground = () => {
-	const router=useRouter();
-	const { data: session } = useSession();
-
-	if(!session?.user){
-		router?.push('/');
-	}
 
 	return (
 		<div className="w-11/12 mx-auto">
